4afd7b98a443e82bce748daa8309c0b7df5043f8
Adam57055 <adamtwam85@gmail.com>
2026-03-14 12:01:40 -0400

    Additional comments for enhanced clarity

--- Programs/odd_or_even.py
+++ Programs/odd_or_even.py
@@ -4,11 +4,9 @@
 
 number = int(input("Which number do you want to check? "))
 
+#If remainder zero after divided by 2, number is even
 if number % 2 == 0:
     print("This is an even number")
+#If remainder not zero after divided by 2, number is even
 else:
     print("This is an odd number")
-
-    
-    
-   
